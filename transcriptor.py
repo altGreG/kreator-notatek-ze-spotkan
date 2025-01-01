04dcf78c7bc3706717b85ad4bac4f00b72c67b92
altGreG <grzeg.bak.pl@gmail.com>
2025-01-01 13:59:27 +0100

    added func for transcribing audio with whisper
    - started writing for alternative transcription tools with access via api

--- transcriptor.py
+++ transcriptor.py
@@ -2,6 +2,11 @@ import platform
 import subprocess
 import os
 
+import whisper
+import torch
+
+import speech_recognition as sr
+
 def extract_audio_from_video(video_file):
     """Ekstrakcja audio z wideo z wykorzystaniem `ffmpeg`"""
     # Sprawdzenie platformy, definicja zmiennej dla kodu z procesu ffmpeg
@@ -11,10 +16,13 @@ def extract_audio_from_video(video_file):
     # Pozyskanie nazwy pliku wideo
     filename, ext = os.path.splitext(video_file)
 
+    # Rozszerzenie dla pliku audio
+    audio_ext = "wav"
+
     # Ekstracja ścieżki audio z pliku i zapis do mp3
     # update_status("Ekstrakcja audio w toku...")
     if system_name == "Windows":
-        extracting_process = subprocess.call(["./software/ffmpeg", "-y", "-i", video_file, f"{filename}.mp3"],
+        extracting_process = subprocess.call(["./software/ffmpeg", "-y", "-i", video_file, f"{filename}.{audio_ext}"],
                         stdout=subprocess.DEVNULL,
                         stderr=subprocess.STDOUT)
     elif system_name == "Linux":
@@ -33,3 +41,36 @@ def extract_audio_from_video(video_file):
 
 # s = "./nagrania/test.mkv"
 # extract_audio_from_video(s)
+
+def transcribe_with_whisper_offline(audio_file):
+    # Wybór jednostki obliczeniowej, procesor lub karta graficzna
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Wybór modelu, do transkrypcji
+    # tiny, base, small, medium, large, turbo
+    model = whisper.load_model("tiny").to(device)
+
+    # przygotowanie ścieżki skąd pobieramy audio, załadowanie pliku audio
+    audio_path = os.path.join(os.path.dirname(__file__), f"nagrania/{audio_file}").replace("\\", "/")
+    audio = whisper.load_audio(audio_path)
+    # audio = whisper.pad_or_trim(audio)
+
+    # Transkrypcja audio, wyświetlenie rezultatu w konsoli
+    result = model.transcribe(audio=audio, language="pl")
+    print(result["text"])
+
+# transcribe_with_whisper_offline("test.mp3")
+
+def transcribe_with_sr(audio_file):
+    r = sr.Recognizer()
+
+    audio_path = os.path.join(os.path.dirname(__file__), f"nagrania/{audio_file}").replace("\\", "/")
+    audio_file = sr.AudioFile(audio_path)
+    with audio_file as source:
+        audio = r.record(source, duration=30)
+        # TODO(altGreG): Get api key to selected transcribe service
+        # text = r.recognize_google_cloud(audio)
+    # print(text)
+
+# transcribe_with_whisper_offline("test.wav")
+# transcribe_with_sr("test.wav")
