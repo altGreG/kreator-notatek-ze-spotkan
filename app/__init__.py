8c99720fdeda29e184f5e61eda2349896ad23cd7
altGreG <grzeg.bak.pl@gmail.com>
2025-02-05 23:46:03 +0100

    updated documentation

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,13 @@
 
 Moduły eksportowane przez tę paczkę:
 
-- `window`: aplikacja GUI, która zarządza interfejsem użytkownika.
-- `recorder`: zestaw funkcji do nagrywania obrazu i dźwięku na komputerze.
-- `transcriptor`: zestaw funkcji do transkrypcji audio oraz formatowania tekstu transkrypcji.
+- `window`: aplikacja GUI umożliwiająca zarządzanie sesjami nagrań i transkrypcji za pomocą intuicyjnego interfejsu użytkownika.
+- `recorder`: zestaw funkcji do nagrywania obrazu i dźwięku na komputerze, w tym wybór obszaru ekranu oraz zapisanie fragmentów audio.
+- `transcriptor`: zestaw funkcji do transkrypcji nagrań audio oraz formatowania uzyskanego tekstu w przejrzystą strukturę.
 - `openai_api`: funkcje do generowania podsumowań transkrypcji za pomocą API OpenAI GPT-4.
 - `mail_sender`: funkcje do wysyłania e-maili z załącznikami, np. z transkrypcjami lub podsumowaniami.
 - `logger`: narzędzie do logowania informacji i komunikatów o błędach w aplikacji.
+- `start_recording_and_screenshots`: moduł koordynujący procesy równoległego nagrywania, wykonywania zrzutów ekranu oraz transkrypcji.
 
 Każdy z modułów odpowiada za określoną funkcjonalność, umożliwiając tworzenie pełnych notatek ze spotkań z wykorzystaniem nagrań, transkrypcji i podsumowań.
 """
--- docs/reference.md
+++ docs/reference.md
@@ -9,12 +9,64 @@ Implementacja funkcjonalności w projekcie `Kreator Notatek ze Spotkań`
 
 
 
-# Moduł recorder.py
----
-::: app.recorder
-
-
-
 # Moduł transcriptor.py
 ---
 ::: app.transcriptor
+
+
+
+# Moduł recorder_audio.py
+---
+::: app.recorder_audio
+
+
+
+# Moduł screenshots.py
+---
+::: app.screenshots
+
+
+
+# Moduł start_recording_and_screenshots.py
+---
+::: app.start_recording_and_screenshots
+
+
+
+## Moduły z utilities
+
+
+
+# Moduł recording_utils.py
+---
+::: app.utilities.recording_utils
+
+
+
+# Moduł saving.py
+---
+::: app.utilities.saving
+
+
+
+# Moduł pdf_generator.py
+---
+::: app.utilities.pdf_generator
+
+
+
+# Moduł mail_sender.py
+---
+::: app.utilities.mail_sender
+
+
+
+# Moduł logger.py
+---
+::: app.utilities.logger
+
+
+
+# Moduł openai_api.py
+---
+::: app.utilities.api.openai_api
